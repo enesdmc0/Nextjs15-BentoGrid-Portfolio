5faaccbc62a370f743481c7a10e9304c8cab1d3d
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2024-10-04 12:29:39 +0300

    x8

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import X8 from "@/components/x8"
+
 export default function Home() {
   return (
     <main className="h-screen bg-[#0d0d0d] p-5">
@@ -30,7 +32,8 @@ export default function Home() {
           <div className="absolute inset-0 bg-bg1 bg-contain opacity-5 pointer-events-none mix-blend-difference	"></div>
         </div>
 
-        <div className="row-span-5 col-span-3 relative border border-[#f8f9f91a] bg-[#f8f9f905] rounded-3xl overflow-hidden">
+        <div className="row-span-5 col-span-3 relative border border-[#f8f9f91a] bg-[#f8f9f905] rounded-3xl overflow-hidden p-2">
+          <X8 />
           <div className="absolute inset-0 bg-bg1 bg-contain opacity-5 pointer-events-none mix-blend-difference	"></div>
         </div>
 
---
+++ components/x8.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import React from "react";
+
+const X8 = () => {
+  return (
+    <>
+      <div className="grid grid-cols-7 gap-2 grid-rows-5 h-full">
+        <div className="text-xs text-white/50 col-span-4 flex items-center justify-center gap-5">
+          <div className="flex items-center gap-5">
+            <button className="p-1 border border-[#f8f9f91a] bg-[#f8f9f905] rounded-md">
+              <Image
+                src="/left.svg"
+                className="text-[#6b7280]"
+                alt="Left Icon"
+                width={16}
+                height={16}
+              />
+            </button>
+            <button className="p-1 border border-[#f8f9f91a] bg-[#f8f9f905] rounded-md">
+              <Image
+                src="/right.svg"
+                alt="Right Icon"
+                width={16}
+                className="text-white/50"
+                height={16}
+              />
+            </button>
+          </div>
+          Thursday 2024
+        </div>
+        {Array(31)
+          .fill(0)
+          .map((_, i) => (
+            <div
+              className="col-span-1 row-span-1 bg-[#f8f9f905] rounded-xl text-white/50 text-xs flex items-center justify-center border border-[#f8f9f91a]"
+              key={i}
+            >
+              {i + 1}
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default X8;
