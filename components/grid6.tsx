e334b6397572f4bd35735db5115f053da8e9f3c0
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2024-10-16 23:41:47 +0300

    bug fixed

--- components/grid6.tsx
+++ components/grid6.tsx
@@ -5,7 +5,7 @@ export const Grid6 = () => {
     return (
         <div className="grid grid-cols-3 grid-rows-3 h-full gap-1">
           {Array(9).fill(0).map((_, i) => (
-            <div key={i} className="h-32 relative w-full md:h-full hover:scale-105 transition-all duration-500">
+            <div key={i} className="h-32 relative w-full lg:h-full hover:scale-105 transition-all duration-500">
               <Image src={`/${i+1}.avif`} alt="" fill className="object-cover  " />
             </div>
           ))}
